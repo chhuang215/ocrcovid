174e87d7cc101a4f3c3ba8c3d71e503ca65d635b
CHHuang <chhuang215@gmail.com>
2020-03-12 02:16:53 +0800

    move client script to assets, some styling

---
+++ src/public/script.ts
@@ -0,0 +1,52 @@
+
+var socket = io.connect('http://localhost:3000');
+
+function getUpdateTimeManually(){
+    var xmlHttp = new XMLHttpRequest();
+    
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+        document.getElementById('updateTime').innerText = 
+            "Last updated: " + new Date(JSON.parse(xmlHttp.responseText)['time']).toLocaleTimeString('en-us');
+        }
+    }
+    xmlHttp.open("GET", 'http://localhost:3000/getupdatetime', true); // true for asynchronous 
+    xmlHttp.send();
+}
+
+function reload(){
+
+    // var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    document.getElementById('loading').innerHTML="";
+    // document.getElementById('ccase').innerHTML = data;
+    
+    document.getElementById('ccaseimg').setAttribute('src', 'ccase.png');
+    document.getElementById('dcaseimg').setAttribute('src', 'dcase.png');
+}
+
+socket.on('updateDataClient', function (updateTime) {
+    reload();
+    document.getElementById('updateTime').innerText = 
+        "Last updated: " + new Date(JSON.parse(updateTime)).toLocaleTimeString('en-us');
+});
+
+
+socket.on('counter', function (count) {
+    if (count > 0){
+        
+        var countFormat = 
+            Math.floor(count / 3600) + ":" + 
+            ('0' +  Math.floor(count % 3600 / 60)).slice(-2)  + ":" + 
+            ('0' + Math.floor(count % 3600 % 60)).slice(-2);
+
+        document.getElementById('counter').innerText = countFormat;
+    }
+    else{
+        document.getElementById('counter').innerText = " REFRESHING..";
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+    getUpdateTimeManually();
+    reload();
+});
